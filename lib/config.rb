e08a84ab796712a7b623c970fdda881615c02e46
Яyan Priebe <rpriebe@me.com>
2015-11-28 22:21:02 -0700

    removed constants, rolled it into config

--- lib/config.rb
+++ lib/config.rb
@@ -1,5 +1,16 @@
 module Evertils
   class Cfg
+    PACKAGE_NAME = "evertils"
+    INSTALLED_DIR = '/Users/prieber/Personal/evertils' #Gem::Specification.find_by_name(Evertils::PACKAGE_NAME).gem_dir
+    LOG_DIR = INSTALLED_DIR + "/logs"
+    DEFAULT_LOG = Log.new # no args means default log
+    HELPER_DIR = INSTALLED_DIR + "/lib/helpers/"
+    CONTROLLER_DIR = INSTALLED_DIR + "/lib/controllers/"
+    MODEL_DIR = INSTALLED_DIR + "/lib/models/"
+    TEMPLATE_DIR = INSTALLED_DIR + "/lib/configs/templates/"
+    LOG_DIGEST_LENGTH = 20
+    DEBUG = false
+    
     def bootstrap!
       begin
         # configure Notifaction gem
