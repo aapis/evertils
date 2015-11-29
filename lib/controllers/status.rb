160193be152448a3e27b4c4ba25144c6137155e5
Яyan Priebe <rpriebe@me.com>
2015-11-28 22:07:04 -0700

    added basic status controller

---
+++ lib/controllers/status.rb
@@ -0,0 +1,30 @@
+module Evertils
+  module Controller
+    class Status < Controller::Base
+      attr_accessor :force, :start
+
+      def pre_exec
+        # all methods require internet to make API calls
+        #@methods_require_internet.push(:daily, :weekly, :monthly, :deployment)
+
+        OptionParser.new do |opt|
+          opt.banner = "#{Evertils::PACKAGE_NAME} generate timeframe [...-flags]"
+
+          opt.on("-f", "--force", "Force execution") do
+            @force = true
+          end
+
+          opt.on("-s", "--start=START", "Specify a date for the note") do |date|
+            @start = DateTime.parse(date)
+          end
+        end.parse!
+
+        super
+      end
+
+      def default
+        Notify.success("hello!")
+      end
+    end
+  end
+end
