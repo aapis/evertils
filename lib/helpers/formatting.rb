635059941f7c7443e20a6f3523cb5fba3998edcf
Ryan Priebe <rpriebe@me.com>
2016-08-05 15:23:47 -0600

    tweaks to make pq command work

--- lib/helpers/formatting.rb
+++ lib/helpers/formatting.rb
@@ -24,18 +24,7 @@ module Evertils
 
       # Template file for note body
       def template_contents
-        tmpls = {
-          :monday => "#{Evertils::TEMPLATE_DIR}#{command.downcase}-monday.enml",
-          :tuesday => "#{Evertils::TEMPLATE_DIR}#{command.downcase}-tuesday.enml",
-          :wednesday => "#{Evertils::TEMPLATE_DIR}#{command.downcase}-wednesday.enml",
-          :thursday => "#{Evertils::TEMPLATE_DIR}#{command.downcase}-thursday.enml",
-          :friday => "#{Evertils::TEMPLATE_DIR}#{command.downcase}-friday.enml",
-          :default => "#{Evertils::TEMPLATE_DIR}#{command.downcase}.enml"
-        }
-
-        template = local_template_override?(tmpls[:default])
-
-        IO.readlines(template, :encoding => 'UTF-8').join("").gsub!("\n", '')
+        IO.readlines(load_template, :encoding => 'UTF-8').join("").gsub!("\n", '')
       end
 
       # Template string for note title
@@ -47,7 +36,8 @@ module Evertils
           :Daily => "Daily Log [#{arg_date.strftime('%B %-d')} - #{dow}]",
           :Weekly => "Weekly Log [#{arg_date.strftime('%B %-d')} - #{end_of_week.strftime('%B %-d')}]",
           :Monthly => "Monthly Log [#{arg_date.strftime('%B %Y')}]",
-          :Deployments => "#{arg_date.strftime('%B %-d')} - #{dow}"
+          :Deployments => "#{arg_date.strftime('%B %-d')} - #{dow}",
+          :'Priority Queue' => "Queue For [#{arg_date.strftime('%B %-d')} - #{dow}]"
         }
       end
 
@@ -60,7 +50,7 @@ module Evertils
 
       #
       # @since 0.3.1
-      def local_template_override?(default)
+      def load_template(default = "#{Evertils::TEMPLATE_DIR}#{command.downcase}.enml")
         return default if $config.custom_templates.nil?
 
         rval = default
