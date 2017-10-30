2cd10e6795f9163173c06bc4ecebf5652f99f9ee
Ryan Priebe <rpriebe@me.com>
2017-10-30 11:02:18 -0600

    custom sections are no longer supported, just use a custom template instead

--- lib/evertils/helpers/api-enml-handler.rb
+++ lib/evertils/helpers/api-enml-handler.rb
@@ -3,8 +3,8 @@ module Evertils
     class ApiEnmlHandler
       #
       # @since 0.3.7
-      def initialize(enml = nil)
-        convert_to_xml(enml) if enml
+      def initialize(config = nil)
+        @config = config
         self
       end
 
@@ -26,14 +26,7 @@ module Evertils
 
         # remove <br> tags
         note_xml.search('br').each(&:remove)
-
-        enml = note_xml.inner_html.to_s
-
-        # append custom sections to the end of the content if they exist
-        return enml if $config.custom_sections.nil?
-
-        enml += to_enml($config.custom_sections[NOTEBOOK_PRIORITY_QUEUE])
-        enml
+        note_xml.inner_html.to_s
       end
 
       #
