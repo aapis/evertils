79a198665ea5ab0f0a9cc02708d83a2807ddb4e6
Ryan Priebe <rpriebe@me.com>
2017-10-30 11:01:51 -0600

    pass config object to all Type instances as we are no longer using global variables, remove test method, remove optionparser block and pre_exec method as the proper value is passed to the function directly now thanks to vermillion-client

--- lib/evertils/controllers/generate.rb
+++ lib/evertils/controllers/generate.rb
@@ -7,56 +7,36 @@ require_relative '../types/monthly'
 module Evertils
   module Controller
     class Generate < Controller::Base
-      attr_accessor :name
-
-      def pre_exec
-        @methods_require_internet.push(:daily, :weekly, :monthly, :mts)
-
-        OptionParser.new do |opt|
-          opt.banner = "#{Evertils::PACKAGE_NAME} generate timeframe [...-flags]"
-
-          opt.on("-n", "--name=NAME", "A name to pass to the script (not all commands support this flag)") do |name|
-            @name = name
-          end
-        end.parse!
-
-        super
-      end
-
       # generate daily notes
       def daily
-        note = Type::Daily.new
+        note = Type::Daily.new(@config)
         note.create
       end
 
       # generate weekly notes
       def weekly
-        note = Type::Weekly.new
+        note = Type::Weekly.new(@config)
         note.create
       end
 
       # generate monthly notes
       def monthly
-        note = Type::Monthly.new
+        note = Type::Monthly.new(@config)
         note.create
       end
 
       # generate monthly task summary templates
-      def mts
-        Notify.error('Name argument is required', {}) if @name.nil?
+      def mts(arg)
+        Notify.error('Name argument is required', {}) if arg.nil?
 
-        note = Type::MonthlyTaskSummary.new(@name)
+        note = Type::MonthlyTaskSummary.new(@config, arg[1])
         note.create
       end
 
       # generate priority queue notes
       def pq
-        note = Type::PriorityQueue.new
+        note = Type::PriorityQueue.new(@config)
         note.create
-      end
-
-      def test
-        puts "yup"
       end
 
       # creates the notes required to start the day
@@ -65,16 +45,16 @@ module Evertils
       #  - weekly (if today is Monday and there isn't a weekly log already)
       #  - monthly (if today is the 1st and there isn't a monthly log already)
       def morning
-        pq = Type::PriorityQueue.new
+        pq = Type::PriorityQueue.new(@config)
         pq.create
 
-        daily = Type::Daily.new
+        daily = Type::Daily.new(@config)
         daily.create
 
-        weekly = Type::Weekly.new
+        weekly = Type::Weekly.new(@config)
         weekly.create if weekly.should_create?
 
-        monthly = Type::Monthly.new
+        monthly = Type::Monthly.new(@config)
         monthly.create if monthly.should_create?
       end
     end
