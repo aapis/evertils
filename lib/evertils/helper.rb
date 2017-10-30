cb0eae0eb5150ac4c830cea369155eb101daf856
Ryan Priebe <rpriebe@me.com>
2017-10-30 10:58:16 -0600

    refactor to handle multiple arguments

--- lib/evertils/helper.rb
+++ lib/evertils/helper.rb
@@ -1,18 +1,18 @@
 module Evertils
   module Helper
-    def self.load(klass, args = nil)
+    def self.load(klass, *args)
       begin
         klass_instance = Evertils::Helper.const_get(klass)
 
         if klass_instance
-          if args.nil?
+          if args.empty?
             klass_instance.new
           else
             klass_instance.new(args)
           end
         end
       rescue => e
-        Notify.error(e.message)
+        Notify.error("#{e}\n#{e.backtrace.join("\n")}", show_time: false)
       end
     end
   end
