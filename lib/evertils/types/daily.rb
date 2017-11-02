2f4734016bfb433dddf4159cdc19cc41fcf02985
Ryan Priebe <rpriebe@me.com>
2017-11-02 14:41:08 -0600

    if PQ note is not found it will throw an error when attempting to get it's GUID, so lets just wait until it is found (NOTE: may eat up a bunch of API calls)

--- lib/evertils/types/daily.rb
+++ lib/evertils/types/daily.rb
@@ -36,8 +36,20 @@ module Evertils
         @api = Evertils::Helper.load('ApiEnmlHandler', @config)
         enml = @api.from_str(@format.template_contents(NOTEBOOK))
 
-        title = @format.date_templates[:'Priority Queue']
-        pq = @model.find_note_contents(title)
+        pq = find_priority_queue
+
+        # didn't find the note the first time?  wait and try again
+        if pq.entity.nil?
+          iter = 0
+          loop do
+            iter += 1
+            pq = find_priority_queue(true)
+            break unless pq.entity.nil?
+          end
+
+          Notify.info("#{iter} attempts to find the pq note") unless iter.zero?
+        end
+
         guid = pq.entity.guid
         user = @model.user_info[:user]
         shard = @model.user_info[:shard]
@@ -48,6 +60,12 @@ module Evertils
 
         enml.at('li:contains("Queue") ul li').children.first.replace(a)
         @content = enml
+      end
+
+      def find_priority_queue(sleep = false)
+        sleep(5) if sleep
+        title = @format.date_templates[:'Priority Queue']
+        @model.find_note_contents(title)
       end
     end
   end
