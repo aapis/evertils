67b27356144b52e00bc10a236e9c36ffbe01714c
Ryan Priebe <rpriebe@me.com>
2019-05-10 13:43:13 -0600

    follow SRP better

--- lib/evertils/controllers/render.rb
+++ lib/evertils/controllers/render.rb
@@ -4,14 +4,18 @@ module Evertils
   module Controller
     class Render < Controller::Base
       def from_file(config)
-        helper = Evertils::Helper.load('Note')
-        note = helper.wait_for(:Daily)
-        link = helper.external_url_for(note.entity)
-
-        return Notify.warning("Note already exists\n- #{link}") if note.exists?
+        return Notify.warning("Note already exists\n- #{@link}") if note_exists?
 
         query = Evertils::Common::Query::Simple.new
         query.create_note_from_yml(config[:path])
+      end
+
+      def note_exists?
+        helper = Evertils::Helper.load('Note')
+        note = helper.wait_for(:Daily, 5)
+        @link = helper.external_url_for(note.entity) unless note.entity.nil?
+
+        note.exists?
       end
     end
   end
