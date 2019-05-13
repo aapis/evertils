e733c326273d4267bba3578f9197269d7b2027d4
Ryan Priebe <rpriebe@me.com>
2019-05-13 10:35:23 -0600

    implement actions

--- lib/evertils/controllers/render.rb
+++ lib/evertils/controllers/render.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
 
-require 'evertils/common/query/simple'
-
 module Evertils
   module Controller
     class Render < Controller::Base
       def from_file(config)
-        @config = config.translate_placeholders.pluck(:title, :notebook)
+        @config = config.translate_placeholders.pluck(:title, :notebook, :path, :action)
 
         return Notify.warning("Note already exists\n- #{@link}") if note_exists?
 
         Notify.info 'Note not found, creating a new one'
 
-        query = Evertils::Common::Query::Simple.new
-        query.create_note_from_yml(@config[:path])
+        execute_action(@config[:action].to_sym || :create)
       end
 
       def note_exists?
@@ -22,6 +19,15 @@ module Evertils
         @link = helper.external_url_for(note.entity) unless note.entity.nil?
 
         note.exists?
+      end
+
+      def execute_action(action)
+        case action
+        when :duplicate_previous
+          Action::DuplicatePrevious.new(@config)
+        else
+          Action::Create.new(@config)
+        end
       end
     end
   end
