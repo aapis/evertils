8c37b8662b2c309676773039ad4adaee54199f2a
Яyan Priebe <rpriebe@me.com>
2020-03-31 17:22:05 -0600

    implement log-group and log-grep to work with data in your daily note (this will be customizable in the future)

--- lib/evertils/controllers/log.rb
+++ lib/evertils/controllers/log.rb
@@ -4,6 +4,7 @@ module Evertils
       def pre_exec
         super
 
+        @note = nil
         @note_helper = Evertils::Helper::Note.instance
         @api_helper = Evertils::Helper::ApiEnmlHandler.new(@config)
       end
@@ -12,11 +13,33 @@ module Evertils
       def message(text = nil)
         return Notify.error('A message is required') if text.nil?
 
-        note = @note_helper.find_note_by_grammar(grammar.to_s)
+        @note = @note_helper.find_note_by_grammar(grammar.to_s)
 
-        return Notify.error('Note not found') if note.entity.nil?
+        return Notify.error('Note not found') if @note.entity.nil?
 
-        modify(note, text)
+        modify_with(text)
+      end
+
+      #
+      # @since 2.2.0
+      def grep(text = nil)
+        return Notify.error('A search term is required') if text.nil?
+
+        @note = @note_helper.find_note_by_grammar(grammar.to_s)
+
+        return Notify.error('Note not found') if @note.entity.nil?
+
+        search_for(text)
+      end
+
+      #
+      # @since 2.2.0
+      def group(text = nil)
+        @note = @note_helper.find_note_by_grammar(grammar.to_s)
+
+        return Notify.error('Note not found') if @note.entity.nil?
+
+        group_by(text)
       end
 
       private
@@ -33,15 +56,13 @@ module Evertils
       end
 
       # Update a note with content
-      def modify(note, text)
-        xml = @api_helper.from_str(note.entity.content)
+      def modify_with(text)
+        xml = @api_helper.from_str(@note.entity.content)
 
         time = Time.now.strftime('%I:%M')
         target = xml.search('en-note').first
 
-        return Notify.error('Unable to log message, triage section not found') if target.nil?
-
-        log_message_txt = "<div>* #{time} - #{text}</div>"
+        log_message_txt = "<div>* #{time} - #{clean(text)}</div>"
 
         # append the log message to the target
         target.add_child(log_message_txt)
@@ -51,9 +72,66 @@ module Evertils
           xml.children[1].remove
         end
 
-        note.entity.content = xml.to_s
+        @note.entity.content = xml.to_s
 
-        Notify.success("Item logged at #{time}") if note.update
+        Notify.success("Item logged at #{time}") if @note.update
+      end
+
+      #
+      # @since 2.2.0
+      def search_for(text)
+        results = grep_results_for(text)
+
+        return Notify.error("No rows matched search query {#{text}}") if results.empty?
+
+        Notify.success("#{results.size} rows matched query {#{text}}")
+        results.each { |res| Notify.info(clean(res)) }
+      end
+
+      #
+      # @since 2.2.0
+      def group_by(text)
+        grouped_results.each_pair do |job_id, rows|
+          Notify.note("#{clean(job_id)} - #{rows.size} occurrences") unless job_id.nil?
+
+          rows.each { |row| Notify.info(clean(row)) }
+        end
+      end
+
+      #
+      # @since 2.2.0
+      def search_nodes
+        xml = @api_helper.from_str(@note.entity.content)
+        target = xml.search('en-note').first
+        nodes = []
+
+        target.children.each do |child|
+          node = child.children.first.to_s
+          nodes.push(clean(node)) unless node.empty? || node == '<br/>'
+        end
+
+        nodes
+      end
+
+      #
+      # @since 2.2.0
+      def grouped_results
+        search_nodes.group_by do |node|
+          match = /- (.*)? -/.match(node)
+          match[1] unless match.nil?
+        end
+      end
+
+      #
+      # @since 2.2.0
+      def grep_results_for(text)
+        search_nodes.select { |line| line.include? text }
+      end
+
+      #
+      # @since 2.2.0
+      def clean(text)
+        text.delete("\n").gsub('&#xA0;', ' ')
       end
     end
   end
