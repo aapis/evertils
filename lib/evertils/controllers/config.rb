222d8b7c45f1aeafccd02eed5c915efed5623ee5
Яyan Priebe <rpriebe@me.com>
2020-04-01 13:21:07 -0600

    the gist gem doesn't really work, forces all data to stdout

---
+++ lib/evertils/controllers/config.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require 'yaml/store'
+
+module Evertils
+  module Controller
+    class Config < Controller::Base
+      def pre_exec
+        conf = contents_of('~/.evertils/config.yml')
+        @token = conf['gist_token'] unless conf['gist_token'].nil?
+        # if the requested gist doesn't exist, ignore it and generate a new one
+        puts @token.inspect
+        @token = nil unless gist_exists?
+        puts gist_exists?
+        puts @token.inspect
+        exit
+
+        Gist.login! unless has_auth_already?
+      end
+
+      def push
+        options = {
+          public: false
+        }
+
+        options.merge!(update: @token) unless @token.nil?
+
+        resp = Gist.multi_gist(payload, options)
+        @token = resp['id'] if resp.key?('id')
+
+        Notify.success("Gist created/updated - #{resp['url']}") if store_token?
+      end
+
+      def pull
+
+      end
+
+      private
+
+      def payload
+        {
+          'config.yml' => contents_of('~/.evertils/config.yml').to_yaml
+        }.merge(types)
+      end
+
+      def contents_of(file)
+        YAML.load_file(File.expand_path(file))
+      end
+
+      def types
+        types = {}
+
+        Dir[File.expand_path('~/.evertils/templates/type/*.yml')].each do |dir|
+          types["templates/type/#{dir.split('/').last}"] = contents_of(dir).to_yaml
+        end
+
+        types
+      end
+
+      def gist_authenticate
+        Gist.login!
+      end
+
+      def gist_exists?
+        # begin
+          Gist.list_all_gists
+          $stdout.flush
+          puts gists.scan(@token).inspect
+
+        # rescue
+        #   false
+        # end
+      end
+
+      def has_auth_already?
+        File.exist?(File.expand_path('~/.gist'))
+      end
+
+      def store_token?
+        store = YAML::Store.new(File.expand_path('~/.evertils/config.yml'))
+        yaml = contents_of('~/.evertils/config.yml')
+
+        store.transaction do
+          yaml.each_pair { |key, value| store[key] = value }
+          store['gist_token'] = @token
+        end
+
+        true
+      end
+    end
+  end
+end
--- lib/evertils/controllers/log.rb
+++ lib/evertils/controllers/log.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Evertils
   module Controller
     Formatting = Evertils::Helper::Formatting
