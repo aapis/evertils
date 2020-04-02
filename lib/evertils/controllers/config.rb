5e011ecd47b83c03b60c396f105ce9e471a4ccbf
Яyan Priebe <rpriebe@me.com>
2020-04-01 21:37:26 -0600

    improved output message, don't include rolling.log since it is created by another client

--- lib/evertils/controllers/config.rb
+++ lib/evertils/controllers/config.rb
@@ -9,8 +9,9 @@ module Evertils
       def pre_exec
         conf = contents_of('~/.evertils/config.yml')
         @token = conf['gist_token'] unless conf['gist_token'].nil?
+        @updating = Gist.gist_exists?(@token)
         # if the requested gist doesn't exist, ignore it and generate a new one
-        @token = nil unless Gist.gist_exists?(@token)
+        @token = nil unless @updating
 
         Gist.login! unless has_auth_already?
       end
@@ -25,7 +26,7 @@ module Evertils
         resp = Gist.multi_gist(payload, options)
         @token = resp['id'] if resp.key?('id')
 
-        Notify.success("Gist created/updated - #{resp['html_url']}") if store_token?
+        Notify.success(message(resp['html_url'])) if store_token?
       end
 
       def pull
@@ -38,7 +39,7 @@ module Evertils
         dir = {}
         t_pfx = '~/.evertils/templates/type'
         c_pfx = '~/.evertils'
-        root_files = ['config.yml', 'rolling.log']
+        root_files = ['config.yml']
 
         files.each_pair do |_, file|
           dir["#{t_pfx}/#{file['filename']}"] = file['content'] unless file['filename'] == 'config.yml'
@@ -52,10 +53,15 @@ module Evertils
 
       private
 
+      def message(url)
+        return "Gist updated - #{url}" if @updating
+
+        "Gist created - #{url}"
+      end
+
       def payload
         {
           'config.yml' => contents_of('~/.evertils/config.yml').to_yaml,
-          'rolling.log' => File.read(File.expand_path('~/.evertils/rolling.log'))
         }.merge(types)
       end
 
